3414d886815a3e0abae4a2985d683f7f89792bfa
Jose Miguel Sanchez <55396436+JosoByte@users.noreply.github.com>
2019-11-01 20:42:57 +0100

    Add files via upload

--- Javascript/tema4/cookies.js
+++ Javascript/tema4/cookies.js
@@ -1,7 +1,18 @@
-cookie=document.cookie.split('=')[1];
-if (cookie!=""){
-	alert("Saludos "+cookie);
+function readCookies(){
+	var cookie = document.cookie;
+	name=cookie.split(';')[0];
+	name=name.split('=')[1];
+	if (cookie!=""){
+		alert("Saludos "+name);
+	}
+	cookie = document.cookie;
+	color=cookie.split(';')[1];
+	color=color.split('=')[1];
+	document.getElementById("bod").style.backgroundColor=color
+	//SETTING SITE COOKIES
 }
+
+
 function readName(){
 	var date = new Date();
 	date.setTime(date.getTime() + (5 * 60 * 1000));
@@ -14,4 +25,19 @@ function checkCookie(){
 }
 function cerrarSesion(){
 	document.cookie = "username=";
+}
+
+function redBackground(){
+	document.cookie = "color=red";
+	cookie = document.cookie;
+	color=cookie.split(';')[1];
+	color=color.split('=')[1];
+	document.getElementById("bod").style.backgroundColor=color
+}
+function blueBackground(){
+	document.cookie = "color=blue";
+	cookie = document.cookie;
+	color=cookie.split(';')[1];
+	color=color.split('=')[1];
+	document.getElementById("bod").style.backgroundColor=color
 }
